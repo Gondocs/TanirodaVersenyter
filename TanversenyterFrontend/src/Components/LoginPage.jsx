602b65db6e54eef6800274ba4b4187e545996fec
Róbert Göndöcs <gondocs.robert@gmail.com>
2024-08-29 16:32:03 +0200

    add register and login

--- TanversenyterFrontend/src/Components/LoginPage.jsx
+++ TanversenyterFrontend/src/Components/LoginPage.jsx
@@ -1,32 +1,85 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { showToast } from "./Toasts/toast";
+import { post } from "./api/api";
+import { useAuth } from "./Auth/Auth";
+import { jwtDecode } from "jwt-decode";
 
 const LoginPage = () => {
+  const { setToken } = useAuth();
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    post
+      .LoginData(formData)
+      .then(({ token }) => {
+        setToken(token);
+        const decoded = jwtDecode(token);
+
+        navigate("/", { replace: true });
+        showToast("Sikeres Bejelentkezés!", "success");
+        console.log(token);
+        console.log(decoded);
+      })
+      .catch((error) => {
+        showToast("Hiba történt a belépés közben: " + error.message, "error");
+        console.log(error);
+      });
+  };
+
   return (
     <div className="h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-96">
-        <h2 className="text-2xl font-bold mb-6 text-center">Belépés</h2>
-        <form>
-          <div className="mb-4">
-            <label className="block text-gray-700">Email</label>
+      <div className="bg-white p-12 rounded-xl shadow-2xl w-full max-w-lg">
+        <h2 className="text-3xl font-bold mb-8 text-center">Belépés</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-lg">Email</label>
             <input
               type="email"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-              placeholder="Enter your email"
+              className="w-full px-4 py-3 text-lg border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+              placeholder="Email"
+              autoComplete="email"
+              required
+              onChange={handleChange}
+              value={formData.email}
+              name="email"
+              id="email"
             />
           </div>
-          <div className="mb-6">
-            <label className="block text-gray-700">Jelszó</label>
+          <div className="mb-8">
+            <label className="block text-gray-700 text-lg">Jelszó</label>
             <input
               type="password"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-              placeholder="Enter your password"
+              className="w-full px-4 py-3 text-lg border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+              placeholder="Jelszó"
+              autoComplete="current-password"
+              required
+              onChange={handleChange}
+              value={formData.password}
+              name="password"
+              id="password"
             />
           </div>
-          <button className="w-full bg-blue-600 text-white py-2 rounded-lg font-semibold hover:bg-blue-700">
+          <button className="w-full bg-blue-600 text-white py-3 rounded-lg font-semibold text-lg hover:bg-blue-700">
             Belépés
           </button>
         </form>
-        <p className="text-center mt-4">
+        <p className="text-center mt-6 text-lg">
           Nincs még fiókod?{" "}
           <a href="/regisztracio" className="text-blue-600 hover:underline">
             Regisztráció
--- TanversenyterFrontend/src/Components/RegistrationPage.jsx
+++ TanversenyterFrontend/src/Components/RegistrationPage.jsx
@@ -1,40 +1,158 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { showToast } from "./Toasts/toast";
+import { validatePassword } from "./Validation/PasswordValidation";
+import { post } from "./api/api";
 
 const RegisterPage = () => {
+  const navigate = useNavigate();
+  const [passwordErrors, setPasswordErrors] = useState([]);
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  const [formData, setFormData] = useState({
+    lastname: "",
+    firstname: "",
+    email: "",
+    password: "",
+    role: "Team",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const passwordErrors = validatePassword(
+      formData.password,
+      passwordConfirmation
+    );
+    setPasswordErrors(passwordErrors);
+
+    if (passwordErrors.length > 0) {
+      showToast(
+        "Hiba a jelszó mezők validálása során. Kérjük, ellenőrizze a hibákat.",
+        "error"
+      );
+      return;
+    }
+
+    try {
+      await post.RegisterData(formData);
+      showToast("Sikeres regisztráció!", "success");
+      navigate("/");
+    } catch (error) {
+      showToast(
+        "Hiba történt a regisztráció közben: " + error.response.data,
+        "error"
+      );
+      console.log(error);
+    }
+
+    console.log("Form Data:", formData);
+  };
+
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-96">
-        <h2 className="text-2xl font-bold mb-6 text-center">Regisztráció</h2>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12">
+      <div className="bg-white p-12 rounded-xl shadow-2xl w-full max-w-lg">
+        <h2 className="text-3xl font-bold mb-8 text-center">Regisztráció</h2>
         <form>
-          <div className="mb-4">
-            <label className="block text-gray-700">Email</label>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-lg">Vezetéknév</label>
+            <input
+              type="text"
+              name="lastname"
+              className="w-full px-4 py-3 text-lg border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+              placeholder="Vezetéknév"
+              autoComplete="family-name"
+              required
+              onChange={handleChange}
+              value={formData.lastname}
+              id="lastname"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-lg">Keresztnév</label>
+            <input
+              type="text"
+              name="firstname"
+              className="w-full px-4 py-3 text-lg border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+              placeholder="Keresztnév"
+              autoComplete="given-name"
+              required
+              onChange={handleChange}
+              value={formData.firstname}
+              id="firstname"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-lg">Email</label>
             <input
               type="email"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-              placeholder="Enter your email"
+              name="email"
+              className="w-full px-4 py-3 text-lg border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+              placeholder="Email"
+              autoComplete="email"
+              required
+              onChange={handleChange}
+              value={formData.email}
+              id="email"
             />
           </div>
           <div className="mb-6">
-            <label className="block text-gray-700">Jelszó</label>
+            <label className="block text-gray-700 text-lg">Jelszó</label>
             <input
               type="password"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+              name="password"
+              className="w-full px-4 py-3 text-lg border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
               placeholder="Jelszó"
+              autoComplete="new-password"
+              required
+              onChange={handleChange}
+              value={formData.password}
+              id="password"
             />
           </div>
-          <div className="mb-6">
-            <label className="block text-gray-700">Jelszó megerősítése</label>
+          <div className="mb-8">
+            <label className="block text-gray-700 text-lg">
+              Jelszó megerősítése
+            </label>
             <input
               type="password"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+              className="w-full px-4 py-3 text-lg border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
               placeholder="Jelszó megerősítése"
+              autoComplete="new-password"
+              required
+              onChange={(e) => setPasswordConfirmation(e.target.value)}
+              value={passwordConfirmation}
+              id="passwordConfirmation"
             />
           </div>
-          <button className="w-full bg-blue-600 text-white py-2 rounded-lg font-semibold hover:bg-blue-700">
-            Register
+          {passwordErrors.length > 0 && (
+            <div className="w-full mb-6">
+              <p className="mb-2">Hiba a regisztráció közben:</p>
+              {passwordErrors.map((error, index) => (
+                <p className="text-red-500" key={index}>
+                  {error}
+                </p>
+              ))}
+            </div>
+          )}
+          <button
+            className="w-full bg-blue-600 text-white py-3 rounded-lg font-semibold text-lg hover:bg-blue-700"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Regisztráció
           </button>
         </form>
-        <p className="text-center mt-4">
+        <p className="text-center mt-6 text-lg">
           Van már fiókod?{" "}
           <a href="/belepes" className="text-blue-600 hover:underline">
             Belépés
